7e2f333b5f9234b65d37e775aedf5bdabbf2e8d4
tanmay2006 <tanmayislam101@gmail.com>
2026-01-29 23:35:38 +0600

    make auth middle ware for protected route

---
+++ src/middlewares/authGaurd.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from "express";
+import { auth as authFromLibs } from "./../libs/auth";
+import { UserRole } from "../generated/prisma/enums";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        role: string;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+const auth = (...role: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await authFromLibs.api.getSession({
+        headers: req.headers as any,
+      });
+      if (!session || !session.user) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      req.user = {
+        id: session.user.id,
+        email: session.user.email!,
+        role: session.user.role,
+        emailVerified: session.user.emailVerified!,
+      };
+      if (!session.user.emailVerified) {
+        return res.status(403).json({
+          success: false,
+          message: "Email verification required. Please verify your email!",
+        });
+      }
+
+      if (role.length && !role.includes(session.user.role as UserRole)) {
+        return res.status(403).json({
+          success: false,
+          message:
+            "Forbidden You don't have permission to access this resources!",
+        });
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+export default auth;
