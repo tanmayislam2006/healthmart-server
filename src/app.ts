b5f1941e90b97b24c996c01ccf8b6e6f13eab798
tanmay2006 <tanmayislam101@gmail.com>
2026-01-30 03:28:31 +0600

    complete admin api endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import globalErrorHandler from "./helper/globalErrorHandler";
 import handleNotFound from "./helper/handleNotFound";
 import { medicineRouter } from "./modules/medicine/medicine.route";
 import { sellerRouter } from "./modules/seller/seller.route";
+import { adminRouter } from "./modules/admin/admin.route";
 const app: Application = express();
 app.use(express.json());
 app.use(
@@ -17,6 +18,7 @@ app.use(
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use('/api/medicine',medicineRouter)
 app.use('/api/seller',sellerRouter)
+app.use('/api/admin',adminRouter)
 app.get("/", (req, res) => {
   res.send("Health Mart Server Is Running");
 });
--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -1,6 +1,50 @@
 import { Router } from "express";
-
+import { adminController } from "./admin.controller";
+import auth from "../../middlewares/authGaurd";
+import { UserRole } from "../../generated/prisma/enums";
 
 const router = Router();
+
+router.get(
+  "/users",
+  auth(UserRole.ADMIN),
+  adminController.getAllUser
+);
+
+router.patch(
+  "/users/:id",
+  auth(UserRole.ADMIN),
+  adminController.updateUser
+);
+
+router.get(
+  "/medicines",
+  auth(UserRole.ADMIN),
+  adminController.getAllMedicines
+);
+
+router.get(
+  "/orders",
+  auth(UserRole.ADMIN),
+  adminController.getAllOrders
+);
+
+router.post(
+  "/categories",
+  auth(UserRole.ADMIN),
+  adminController.createCategory
+);
+
+router.get(
+  "/categories",
+  auth(UserRole.ADMIN),
+  adminController.getAllCategories
+);
+
+router.patch(
+  "/categories/:id",
+  auth(UserRole.ADMIN),
+  adminController.updateCategory
+);
 
 export const adminRouter = router;
