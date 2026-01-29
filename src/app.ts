d5c19ba7610da84e07e15f705861e0f4922697ac
tanmay2006 <tanmayislam101@gmail.com>
2026-01-30 00:13:05 +0600

    added the http status and the 404 handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from "express";
 import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./libs/auth";
+import globalErrorHandler from "./helper/globalErrorHandler";
+import handleNotFound from "./helper/handleNotFound";
 const app: Application = express();
 app.use(express.json());
 app.use(
@@ -14,4 +16,6 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 app.get("/", (req, res) => {
   res.send("Health Mart Server Is Running");
 });
+app.use(handleNotFound)
+app.use(globalErrorHandler)
 export default app;
